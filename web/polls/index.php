61f2fbfd443dd8a0709376b599e1d1ac5e292923
Jason Presher <Jason Presher>
2016-10-01 15:46:07 -0500

    polls

--- web/polls/index.php
+++ web/polls/index.php
@@ -2,7 +2,7 @@
 ?>
 <h1>Welcome To My Poll</h1>
 	<p>Please choose a detination</p>
-		<a href= "https://shrouded-shore-88291.herokuapp.com/polls/poll_action.php">Take Poll</a>
+		<a href= "https://shrouded-shore-88291.herokuapp.com/polls/ballot.php">Take Poll</a>
 		<a href= "https://shrouded-shore-88291.herokuapp.com/polls/poll_results.php">See Poll Results</a>
 <?php require_once('footer.php');
 ?>
