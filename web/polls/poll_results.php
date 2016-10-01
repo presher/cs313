a4ab8bc4f487d292004183721fa15f3eac46ca63
Jason Presher <Jason Presher>
2016-10-01 15:20:45 -0500

    polls

--- web/polls/poll_results.php
+++ web/polls/poll_results.php
@@ -1,4 +1,4 @@
-<?php if (!isset($_SESSION)) session_start();
+<?php if (!isset($_SESSION)) session_start();?>
 
  <?php require_once('form_action.php');?>
  <?php require_once('poll_write_file.php'); 
