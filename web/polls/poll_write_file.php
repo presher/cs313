285df5f8d2f3e8f50dbdedc9537c735d0311b398
Jason Presher <Jason Presher>
2016-10-02 08:39:30 -0500

    polls

--- web/polls/poll_write_file.php
+++ web/polls/poll_write_file.php
@@ -3,7 +3,7 @@
 
 //chmod("poll_results.txt",0600);
 //file_put_contents("poll_results.txt", $_POST['polls'], FILE_APPEND) ;
-$filename = "poll_results.php";
+$filename = "poll_results.txt";
 $data = $_POST['polls'];
 function file_put_contents($filename, $data, FILE_APPEND) 
 { 
