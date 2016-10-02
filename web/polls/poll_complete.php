a092cf7ef87c1f7ca6f2d081268553e82d2026bf
Jason Presher <Jason Presher>
2016-10-01 20:36:22 -0500

    polls

---
+++ web/polls/poll_complete.php
@@ -0,0 +1,33 @@
+<?php require_once('header.php'); ?>
+<h1>Poll Complete</h1>
+<div class="container">
+
+<h1>Poll Results</h1>
+	<div class="row">
+        <div class="col-md-12">
+            <div class="boxed">
+
+<table>
+	<thead>
+		<tr>
+			<th>Favorite Book</th>
+			<th>Place you would like to visit</th>
+			<th>Favorite Season</th>
+			<th>Favorite Tree</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td><?php echo get_form_request('favoriteBook'); ?></td>
+			<td><?php echo get_form_request('visit'); ?></td>
+			<td><?php echo get_form_request('favoriteSeason'); ?></td>
+			<td><?php echo get_form_request('favoriteTree'); ?></td>
+		</tr>
+	</tbody>
+</table>
+			</div>
+		</div>
+	</div>
+</div>
+<a href="https://shrouded-shore-88291.herokuapp.com/polls/index.php">Home</a>
+<?php require_once('footer.php');?>
