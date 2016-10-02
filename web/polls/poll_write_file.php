d27e3d412c1a94263a067f03c3932ba80d065522
Jason Presher <Jason Presher>
2016-10-02 08:45:02 -0500

    polls

--- web/polls/poll_write_file.php
+++ web/polls/poll_write_file.php
@@ -5,7 +5,7 @@
 //file_put_contents("poll_results.txt", $_POST['polls'], FILE_APPEND) ;
 $filename = "poll_results.txt";
 $data = $_POST['polls'];
-function file_put_contents($filename, $data, FILE_APPEND) 
+function file_put_contents($filename, $data) 
 { 
   if( $file = fopen($filename, 'w') ) 
   { 
