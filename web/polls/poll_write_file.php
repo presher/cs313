9dd7d30ae25d64d6bcd25c3cde6fea8cd6b0d362
Jason Presher <Jason Presher>
2016-10-01 20:08:58 -0500

    polls

--- web/polls/poll_write_file.php
+++ web/polls/poll_write_file.php
@@ -4,7 +4,7 @@ date_default_timezone_set('America/Denver');
 /**
  * Writes an array of data to a given point in a file.
  */
-/*function write_request_to_file(&$file_ptr, $request) {
+function write_request_to_file(&$file_ptr, $request) {
     foreach ($request as $key => $val) {
         if (!is_array($val)) {
             if (substr($key, 0, 1) !== '_') {
@@ -14,7 +14,7 @@ date_default_timezone_set('America/Denver');
             write_request_to_file($file_ptr, $val);
         }
     }
-}*/
+}
 $file = 'polls/poll_results.txt';
 $today = date("F j, Y, g:i a");
 $current = file_get_contents($file);
