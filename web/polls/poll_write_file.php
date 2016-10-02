fb3901a1a43f21e894b8664114f9de0acaecc4ca
Jason Presher <Jason Presher>
2016-10-02 08:47:44 -0500

    polls

--- web/polls/poll_write_file.php
+++ web/polls/poll_write_file.php
@@ -1,19 +1,12 @@
 <?php
 
 
-//chmod("poll_results.txt",0600);
+chmod("poll_results.txt",0600);
 //file_put_contents("poll_results.txt", $_POST['polls'], FILE_APPEND) ;
 $filename = "poll_results.txt";
 $data = $_POST['polls'];
-function file_put_contents($filename, $data) 
-{ 
-  if( $file = fopen($filename, 'w') ) 
-  { 
-     
-    fclose($file); 
-  } 
-  
-}   
+ file_put_contents($filename, $data, FILE_APPEND); 
+
                    
   
 
