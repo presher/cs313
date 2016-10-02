21a2c07321bf5606a5c72c346b9cd42557bab184
Jason Presher <Jason Presher>
2016-10-02 08:27:35 -0500

    polls

--- web/polls/poll_write_file.php
+++ web/polls/poll_write_file.php
@@ -1,8 +1,21 @@
 <?php
 
 
-chmod("poll_results.txt",0600);
-file_put_contents("poll_results.txt", $_POST['polls'], FILE_APPEND) ;
-
+//chmod("poll_results.txt",0600);
+//file_put_contents("poll_results.txt", $_POST['polls'], FILE_APPEND) ;
+$filename = "poll_results.php";
+$data = $_POST['polls'];
+function file_put_contents($filename, $data, FILE_APPEND) 
+{ 
+  if( $file = fopen($filename, 'w') ) 
+  { 
+    $bytes = fwrite($file, is_array($data) ? implode('', $data) : $data); 
+    fclose($file); return $bytes; // return the number of bytes written to the file 
+  } 
+}   
+                    if(isset($_POST['polls'])) 
+{ 
+    file_put_contents('polls', @file_get_contents('polls') + 1); 
+}   
 
 ?>
