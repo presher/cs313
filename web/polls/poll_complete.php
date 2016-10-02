b50bdfa1847cf9cf47e8d37ef30ea238d5e1c7b9
Jason Presher <Jason Presher>
2016-10-01 20:41:28 -0500

    polls

--- web/polls/poll_complete.php
+++ web/polls/poll_complete.php
@@ -1,8 +1,8 @@
 <?php require_once('header.php'); ?>
-<h1>Poll Complete</h1>
+
 <div class="container">
 
-<h1>Poll Results</h1>
+<h1>Poll Complete</h1>
 	<div class="row">
         <div class="col-md-12">
             <div class="boxed">
